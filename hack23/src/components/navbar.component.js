82648c4dd326d7d2961db85ad1a0696b6e5bec02
Barkın Bryce <2452842b@student.gla.ac.uk>
2023-10-28 13:01:11 +0100

    navbar

--- hack23/src/components/navbar.component.js
+++ hack23/src/components/navbar.component.js
@@ -1,1 +1,20 @@
-// used to hold the navbar and links to other pages
+// Navbar.js
+
+import React from 'react';
+import '../App.css'; // Import your CSS file for styling
+import Logo from '../secondary.png';
+
+function Navbar() {
+  return (
+    <nav className="navbar">
+      <div className="logo"></div>
+      <ul className="nav-links">
+        <li><a href="home.component.js"><img src={Logo} ></img></a></li>
+        <li><a href="products.component.js">Products</a></li>
+        <li><a href="orders.component.js">Orders</a></li>
+      </ul>
+    </nav>
+  );
+}
+
+export default Navbar;
