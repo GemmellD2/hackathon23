5c52fda4030a1434aa77c62a02ada7a32dc1655e
2577007r <2577007r@student.gla.ac.uk>
2023-10-28 16:25:43 +0100

    Added function to fetch all products

--- hack23/src/components/products.component.js
+++ hack23/src/components/products.component.js
@@ -1,12 +1,19 @@
 //will show a view of all orders which should be able to be sorted/filtered
 import React, { Component } from 'react';
 import '../App.css';
+const API_URL = "http://localhost:8000/api/products"
+
+async function getAllProducts() {
+    const res = await fetch(API_URL);
+    const object = await res.json();
+    console.log(object);
+    return object;
+}   
 
 export default class Products extends Component {
 
-
-
     render() {
+        getAllProducts();
         return (
             <div>
                 <h1>Products</h1>
