bccf0f0b43b5b0e829cef319141361ae5ac902a9
David Gemmell <2559087G@student.gla.ac.uk>
2023-10-28 16:17:24 +0100

    fetch implemented for product

--- hack23/src/components/product.component.js
+++ hack23/src/components/product.component.js
@@ -2,6 +2,22 @@
 import React from 'react';
 import '../App.css';
 
+var productData 
+const id = window.location.pathname.split("/")[2]
+const res = await fetch("http://localhost:8000/api/products/getProduct/" + id)
+        .then(response => response.json())
+        .then(data => {
+            productData = data
+        });
+
+
 export const Product = () => {
-        return <h1>Products id:</h1>
+    
+    // Send a POST request with JSON data to https://example.com/api
+    console.log(productData)
+        return (
+            <div>
+                <h1>{productData[0].ItemName}</h1>
+            </div>
+        )
 }
