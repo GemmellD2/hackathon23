96e66850eba12df9529e504e72361f80c02983f0
Daniel <2569281t@student.gla.ac.uk>
2023-10-28 23:24:04 +0100

    login fomr

--- hack23/src/components/login.component.js
+++ hack23/src/components/login.component.js
@@ -1,13 +1,41 @@
-import React, { Component } from 'react';
-import '../App.css';
-
-export default class Login extends Component {
-
-    render() {
-        return (
-            <div>
-                <h1>Log in page.</h1>
-
-            </div>
-        )}
-}
+import {
+    Card,
+    Input,
+    Checkbox,
+    Button,
+    Typography,
+  } from "@material-tailwind/react";
+   
+  export function Login() {
+    return (
+        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
+       <Card color="transparent" shadow={false}>
+        <Typography variant="h4" color="blue-gray">
+          Sign Up
+        </Typography>
+        <Typography color="gray" className="mt-1 font-normal">
+          Nice to meet you! Enter your details to register.
+        </Typography>
+        <form className="mt-8 mb-2 w-80 max-w-screen-lg sm:w-96">
+          <div className="mb-1 flex flex-col gap-6">
+            <Typography variant="h6" color="blue-gray" className="-mb-3">
+              Your UserID
+            </Typography>
+            <Input
+              size="lg"
+              placeholder="name@mail.com"
+              className=" !border-t-blue-gray-200 focus:!border-t-gray-900"
+              labelProps={{
+                className: "before:content-none after:content-none",
+              }}
+            />
+          </div>
+          <Button className="mt-6" fullWidth>
+            sign up
+          </Button>
+        </form>
+      </Card>
+      </div>
+    );
+  }
+  export default Login;
