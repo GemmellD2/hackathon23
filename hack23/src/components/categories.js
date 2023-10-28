5f1344bf82ae8fde1947eb6b83cc4d05e94740db
2577007r <2577007r@student.gla.ac.uk>
2023-10-28 22:42:52 +0100

    Added categories on product page

---
+++ hack23/src/components/categories.js
@@ -0,0 +1,13 @@
+const categories = {
+    'ACGB': 'Accessory - Gig Bags',
+    'ACSTD': 'Accessory Stands',
+    'DDK': 'Digital Drums Kits',
+    'DKTS': 'Keyboard Stands',
+    'DKZ': 'Keyboard Synthesizers',
+    'GU': 'Guitars',
+    'GUAG': 'Acoustic Guitars',
+    'GUBA': 'Guitars Bass',
+    'PEDL': 'Pedals',
+};
+
+export default categories;
--- hack23/src/components/products.component.js
+++ hack23/src/components/products.component.js
@@ -2,9 +2,9 @@
 import React, { Component, useEffect, useState } from 'react';
 import '../App.css';
 import { getAllProducts } from '../services';
+import categories from './categories';
 
 const allProducts = await getAllProducts();
-
 const ProductCard = (key, data) => { 
     return (
         <li key={data.SKU_ID} className="card" >
@@ -17,27 +17,56 @@ const ProductCard = (key, data) => {
     )
 } 
 
-const Product = () => {
+function Product() {
+    const [selectedCategory, setSelectedCategory] = useState('all');
+    const [filteredProducts, setFilteredProducts] = useState([]);
 
-    console.log(allProducts)
+    useEffect(() => {
+        if (selectedCategory === 'all') {
+          // If no category is selected, display all products
+          setFilteredProducts(allProducts);
+        } else {
+          // Filter products based on the selected category
+          const filtered = allProducts.filter((product) => product.Category.startsWith(selectedCategory));
+          setFilteredProducts(filtered);
+        }
+      }, [selectedCategory, filteredProducts]);
 
     return (
         <div class="w-4/5 mx-auto my-10">
-            <h1 class="text-3xl font-bold">Products</h1>
+            <div class='flex flex-row justify-between mt-5'>
+                <h1 class="text-3xl font-bold">Products</h1>
+                <div class='order-last flex flex-col '>
+                    <label htmlFor="categorySelect" class='font-sm font-bold'>Select a Category:</label>
+                    <select id='categorySelect'
+                    onChange={(event) => setSelectedCategory(event.target.value)}
+                    class="block appearance-none w-full bg-white border border-gray-300 hover:border-gray-400 px-4 py-2 pr-8 rounded-lg focus:outline-none focus:border-indigo-500 focus:shadow-outline-indigo active:bg-gray-100"
+                    >
+                        <option value='all' class="text-l">All products</option>
+                            {Object.keys(categories).map((category, index) => (
+                                <option class="text-l" key={index} value={category}>
+                                    {categories[category]}
+                                </option>
+                            ))}
+                    </select>
+                </div>
+            </div>
             <h5 class="text-l text-gray-600">Our current product collection!</h5>
             <div class="flex flex-row my-5 flex-wrap justify-around">
-                { allProducts ?
-                allProducts.map(product => (
+                { filteredProducts.length > 0 ?
+                filteredProducts.map(product => (
                     // <ProductCard key={product.SKU_ID} data={product}/>
                     <a href={`/products/${product.SKU_ID}`} key={product.SKU_ID} className="card" class="relative block w-3/12 h-300 mx-3 my-2 p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100" >
                         <div class="block top-0 right-0 bottom-0 left-0 ease-in-out duration-300 hover:scale-110">
                             <img class="h-40 mx-auto" src={product.PictureMain}/>
                             <h5 class="mb-2 text-center text-xl font-bold tracking-tight text-gray-900 dark:text-white">{product.ItemName}</h5>
-                            <p class="text-center font-normal text-gray-700 dark:text-gray-400">£{product.SalesPrice}</p>
+                            {product.QtyInStock > 0 ? (<p class="text-center font-small text-green-700 mt-0">In Stock</p>) :
+                            (<p class="text-center font-small text-red-700 mt-2">Not In Stock</p>) }
+                            <p class="text-center font-normal text-gray-700 dark:text-gray-400 mt-3">£{product.SalesPrice}</p>
                         </div>
                     </a>
                 ))
-                : <p>Products could not be rendered</p>}
+                : <h1 class="center text-2xl font-bold">No products available in this category.</h1>}
             </div>
         </div>
     )
