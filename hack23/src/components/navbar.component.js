6566e7c57ce07e2e56fe9f95a168909ac167f428
Alex Chudíc <alexc@MacBook-Air-3.local>
2023-10-29 08:19:52 +0000

    Navbar changes

--- hack23/src/components/navbar.component.js
+++ hack23/src/components/navbar.component.js
@@ -26,10 +26,10 @@ export function NavbarWithSearch() {
   }, []);
  
   return (
-    <Navbar className="mx-auto px-4 py-2 lg:px-8 lg:py-4">
-      <div className="container mx-auto flex flex-wrap items-center justify-between text-blue-gray-900">
+    <Navbar className="rounded-none max-w-none mx-auto px-4 py-2 lg:px-0 lg:py-4">
+      <div className="container w-4/5 max-w-screen-2xl mx-auto flex flex-wrap items-center justify-between text-blue-gray-900">
         <a href="https://www.guitarguitar.co.uk/">
-        <img src={Logo} alt="Guitar Guitar Logo"></img>
+          <img src={Logo} alt="Guitar Guitar Logo"></img>
         </a>
         <div className="hidden lg:block">{
           <ul className="mt-2 mb-4 flex flex-col gap-2 lg:mb-0 lg:mt-0 lg:flex-row lg:items-center lg:gap-6">
@@ -82,7 +82,7 @@ export function NavbarWithSearch() {
           </Typography>
         </ul>
         }</div>
-        <div className="hidden items-center gap-x-2 lg:flex">
+        <div className="hidden items-center gap-x-2 xl:flex">
           <div className="relative flex w-full gap-2 md:w-max">
             <Searchbar></Searchbar>
             <div className="!absolute left-3 top-[13px]">
